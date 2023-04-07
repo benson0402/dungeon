b72cca382d641b1c7446b627802e89a28dfed28a
benson0402 <benson930402@gmail.com>
2023-04-07 16:43:17 +0800

    bye

--- src/inventory/skill.cpp
+++
@@ -1,8 +0,0 @@
-#include "inventory/skill.hpp"
-
-
-Skill::Skill(std::string _name, int _atk, Element element) {
-    this->SetName(_name);
-    this->SetAtk(_atk);
-    this->SetElementType(element);
-}
